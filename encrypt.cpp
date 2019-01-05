13de45e6bb387d315aeea3a47d97a032a923bf4d
Tuukka Karvonen <tuukkak@gmail.com>
2019-01-05 13:05:50 -0800

    separate encryption from key generation

--- encrypt.cpp
+++ encrypt.cpp
@@ -10,40 +10,37 @@
 int main(int argc, char ** argv) {
 
     if(argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " <input_decrypted_file> <output_encrypted_file> <output_key_file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input_decrypted_file> <output_encrypted_file> <input_key_file>" << std::endl;
         exit(0);
     }
     
-    CryptoPP::AutoSeededRandomPool rnd;
-
+    std::ifstream inf(argv[3]);
+    std::string keyiv;
+    inf >> keyiv;
     CryptoPP::SecByteBlock key(0x00, CryptoPP::AES::DEFAULT_KEYLENGTH);
-    rnd.GenerateBlock( key, key.size() );
 
     CryptoPP::SecByteBlock iv(CryptoPP::AES::BLOCKSIZE);
-    rnd.GenerateBlock(iv, iv.size());
+
+    unsigned int c;
+    for(int j=0;j<CryptoPP::AES::DEFAULT_KEYLENGTH;j++) {
+        std::string subs = keyiv.substr(j*2, 2);
+        key[j] = strtol(subs.c_str(), NULL, 16);
+    }
+
+    for(int j=0;j<CryptoPP::AES::DEFAULT_KEYLENGTH;j++) {
+        std::string subs = keyiv.substr(CryptoPP::AES::DEFAULT_KEYLENGTH*2 + j*2, 2);
+        iv[j] = strtol(subs.c_str(), NULL, 16);
+    }
 
     CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
     CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, iv );
-
 
     CryptoPP::FileSource fsource (argv[1],
             true,
             new CryptoPP::StreamTransformationFilter(cbcEncryption, new CryptoPP::FileSink(argv[2], true)),
             true);
 
-
     fsource.PumpAll();
-
-    std::ofstream out(argv[3]);
-    for(int j=0;j<CryptoPP::AES::DEFAULT_KEYLENGTH;j++) {
-        out << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(key[j]);
-    }
-
-    for(int j=0;j<CryptoPP::AES::DEFAULT_BLOCKSIZE;j++) {
-        out << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(iv[j]);
-    }
-
-    out.close();
 
     return 0;
 
